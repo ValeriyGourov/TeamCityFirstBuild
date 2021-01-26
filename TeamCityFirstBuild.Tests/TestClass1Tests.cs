bf365d1b8b4d3aafbf5663e28f5ceaf18a2f0027
Валерий Гуров <ValeriyGourov@live.ru>
2021-01-26 22:54:34 +0200

    Добавлены тесты

---
+++ TeamCityFirstBuild.Tests/TestClass1Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeamCityFirstBuild.Tests
+{
+	[TestClass]
+	public class TestClass1Tests
+	{
+		[TestMethod]
+		public void TestMethod1Test()
+		{
+			// Arrange.
+			TestClass1 testClass1 = new();
+			const int
+				arg1 = 4,
+				arg2 = 5,
+				expected = 9;
+
+			// Act.
+			int actual = testClass1.TestMethod1(arg1, arg2);
+
+			// Assert.
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ TeamCityFirstBuild/TestClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamCityFirstBuild
+{
+	public class TestClass1
+	{
+		public int TestMethod1(int arg1, int arg2) => arg1 + arg2;
+	}
+}
