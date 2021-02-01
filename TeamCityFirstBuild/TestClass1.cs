a1173d8f6f1435dbeec176479275f30a85cc41c3
Валерий Гуров <ValeriyGourov@live.ru>
2021-02-01 10:47:44 +0200

    Проверка покрытия кода

--- TeamCityFirstBuild/TestClass1.cs
+++ TeamCityFirstBuild/TestClass1.cs
@@ -1,7 +1,17 @@
-﻿namespace TeamCityFirstBuild
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace TeamCityFirstBuild
 {
 	public class TestClass1
 	{
+		public int MyProperty1 { get; set; }
+
 		public int TestMethod1(int arg1, int arg2) => arg1 + arg2;
+
+		[ExcludeFromCodeCoverage]
+		public int TestMethod2(int arg1, int arg2) => arg1 - arg2;
+
+		public async Task<int> TestMethod3(int arg1, int arg2) => await Task.FromResult<int>(arg1 % arg2);
 	}
 }
