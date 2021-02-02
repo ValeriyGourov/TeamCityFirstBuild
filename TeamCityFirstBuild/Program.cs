22ff91a58e5ec6d2c8286b0317f8abcaa8b95d33
Валерий Гуров <ValeriyGourov@live.ru>
2021-02-02 23:00:18 +0200

    Добавлены пакеты NuGet

--- TeamCityFirstBuild/Program.cs
+++ TeamCityFirstBuild/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+using Serilog;
+
 namespace TeamCityFirstBuild
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Log.Logger = new LoggerConfiguration().CreateLogger();
+			Log.Information("No one listens to me!");
+
 			Console.WriteLine("Hello World!");
+
+			// Finally, once just before the application exits...
+			Log.CloseAndFlush();
 		}
 	}
 }
